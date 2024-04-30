5ed7acac366513eb534b76c9a914e6b41afba754
ojds2022 <odiazstrandberg@gmail.com>
2024-04-29 21:00:12 -0700

    feat: added more comments

--- assets/endPage.js
+++ assets/endPage.js
@@ -1,7 +1,7 @@
 const userFinalScore = document.getElementById('userFinalScore');
 const score = localStorage.getItem('userScore');
 
-function displayFinalScore() {
+function displayFinalScore() { // displays the user's final score on the last page
     if (userFinalScore) {
         userFinalScore.innerText = `Your score: ${score}`;
     }
--- assets/highScores.js
+++ assets/highScores.js
@@ -4,7 +4,7 @@ const parsedArray = JSON.parse(highScoresArray);
 const sortedArray = parsedArray.sort((a, b) => b - a); // sorts the array in descending order
 const threeHighestScores = sortedArray.slice(0, 3); // takes the first three elements from the sorted array
 
-function displayHighScores() {
+function displayHighScores() { // displays the user's top three scores on the high scores page
     if (userHighScores) {
         userHighScores.innerText = `Your top 3 scores: ${threeHighestScores}`;
     }
